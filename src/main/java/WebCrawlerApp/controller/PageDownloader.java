f3914fc03ea64a1ca22abff731193f53d734387f
JuliaZur <julia17044@gmail.com>
2018-11-20 10:15:34 +0100

    new gitignore

--- src/main/java/WebCrawlerApp/controller/PageDownloader.java
+++ src/main/java/WebCrawlerApp/controller/PageDownloader.java
@@ -1,11 +1,17 @@
 package WebCrawlerApp.controller;
 
 import WebCrawlerApp.model.Page;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import java.io.IOException;
+import static com.sun.activation.registries.LogSupport.log;
 
 public class PageDownloader {
 
-    private Page downloadPage(String URL){
-        return new Page();
+    private Page downloadPage(String URL,Integer depth) throws IOException{
+        Document doc = Jsoup.connect(URL).get();
+        Page page = new Page(URL,"",depth);
+        return page;
     }
 
     private void getURLs(){
--- src/main/java/WebCrawlerApp/model/Page.java
+++ src/main/java/WebCrawlerApp/model/Page.java
@@ -4,6 +4,12 @@ import javafx.scene.input.DataFormat;
 
 public class Page {
     String URL;
-    DataFormat signature;
+    String signature;
     Integer depth;
+
+    public Page(String URL, String signature,Integer depth){
+        this.URL = URL;
+        this.signature = signature;
+        this.depth = depth;
+    }
 }

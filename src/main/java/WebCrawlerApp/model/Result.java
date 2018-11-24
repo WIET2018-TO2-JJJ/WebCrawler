52909a9e189f89a80b788e4513fd14bcb0f67526
JuliaZur <julia17044@gmail.com>
2018-11-24 15:18:23 +0100

    Prepared base search.

--- src/main/java/WebCrawlerApp/model/Result.java
+++ src/main/java/WebCrawlerApp/model/Result.java
@@ -3,4 +3,9 @@ package WebCrawlerApp.model;
 public class Result {
     String URL;
     String sentence;
+
+    public Result(String URL, String sentence){
+        this.URL = URL;
+        this.sentence = sentence;
+    }
 }
--- src/main/java/WebCrawlerApp/model/Search.java
+++ src/main/java/WebCrawlerApp/model/Search.java
@@ -5,29 +5,66 @@
 package WebCrawlerApp.model;
 
 
+import WebCrawlerApp.controller.PageDownloader;
+import WebCrawlerApp.controller.PageParser;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
-
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Search {
 
-    List<Page> pagesToVisit;
+    List<String> pagesToVisit;
     HashMap<String,Page> pagesVisited;
     Integer acctualDepth;
+    String queryPositive;
+    String queryNegative;
     private StringProperty name;
 
-    public Search(String name) {
+    public Search(String name, String queryPositive, String queryNegative, Integer acctualDepth) {
+
         this.name = new SimpleStringProperty(name);
+        this.pagesToVisit = new LinkedList<>();
+        this.pagesVisited = new HashMap<>();
+        this.queryPositive = queryPositive;
+        this.queryNegative = queryNegative;
+        this.acctualDepth = acctualDepth;
     }
 
     public final StringProperty getNameProperty() {
         return name;
     }
 
-    private void search(){
+    private void search() throws IOException{
 
+        while (!pagesToVisit.isEmpty()) {
+
+            if (!pagesVisited.containsKey(pagesToVisit.get(0))){
+
+                PageDownloader pageDownloader = new PageDownloader();
+                Document doc = pageDownloader.downloadPage(pagesToVisit.get(0), acctualDepth);
+                String signature = pageDownloader.getSignature();
+                Page page = new Page(pagesToVisit.get(0),signature,acctualDepth);
+                pagesToVisit.remove(0);
+                pagesVisited.put(page.URL,page);
+
+                Elements links = pageDownloader.getURLs();
+                PageParser pageParser = new PageParser(queryPositive,queryNegative,doc);
+                for (Element link : links){
+                    if (pageParser.validateUrl(link))
+                        pagesToVisit.add(link.absUrl("href"));
+                }
+                List<String> sentences = pageParser.searchForWords(doc);
+                for (String sentence : sentences){
+                    Result result = new Result(page.URL,sentence);
+                }
+            }
+        }
     }
 }

d8200bf00f4e2dbed471cefea262aad0fec53cad
JuliaZur <34189609+JuliaZur@users.noreply.github.com>
2018-11-26 14:19:02 +0100

    Update PageTest.java

--- src/test/java/PageTest.java
+++ src/test/java/PageTest.java
@@ -1,18 +1,23 @@
 import WebCrawlerApp.controller.PageDownloader;
 import WebCrawlerApp.controller.PageParser;
+import WebCrawlerApp.controller.PatternMatcher;
+import WebCrawlerApp.controller.pattern.Pattern;
 import com.sun.org.apache.xerces.internal.util.SynchronizedSymbolTable;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Element;
+import org.jsoup.safety.Whitelist;
 import org.jsoup.select.Elements;
 import org.junit.Before;
 import org.junit.Test;
 import org.jsoup.nodes.Document;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
-
 import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.format;
 
 public class PageTest {
 
@@ -26,9 +31,6 @@ public class PageTest {
         File input = new File("./pageForTest/Wikipedia:Strona główna.html");
         doc = Jsoup.parse(input, "UTF-8", "https://pl.wikipedia.org");
         pageDownloader.setDocument(doc);
-        String queryPositive = "Franklin";
-        String queryNegative = null;
-        pageParser = new PageParser(queryPositive,queryNegative,doc);
     }
 
     @Test
@@ -43,18 +45,39 @@ public class PageTest {
 
     @Test
     public void searchForWordsTest(){
-        List<String> results = pageParser.searchForWords(doc);
-        assertEquals(results.size(),4);
+        Elements body = doc.body().select("p,li").append("\n");
+        List<String> sentences = new ArrayList<>();
+        for (Element e : body){
+            sentences.add(e.text());
+        }
+        for (String o : sentences){
+            System.out.println(o);
+            System.out.println();
+        }
+
+        Pattern positive = new Pattern("był");
+        List<String> results = PatternMatcher.matchAgainstPatterns(sentences,positive,null);
+        assertEquals(results.size(),5);
     }
 
     @Test
     public void searchForWordsMatchEverythingTest(){
+        String body = doc.body().text();
+        List<String> sentences = Arrays.asList(body.split("\\. | • "));
 
+        Pattern positive = new Pattern("*");
+        List<String> results = PatternMatcher.matchAgainstPatterns(sentences,positive,null);
+        assertEquals(results.size(),sentences.size());
     }
 
     @Test
     public void searchForWordsMatchNothingTest(){
+        String body = doc.body().text();
+        List<String> sentences = Arrays.asList(body.split("\\. | • "));
 
+        Pattern negative = new Pattern("*");
+        List<String> results = PatternMatcher.matchAgainstPatterns(sentences,null,negative);
+        assertEquals(results.size(),0);
     }
 
 }
